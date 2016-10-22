7c6965e25bec2931c66547e17b09c760a037f5d2
Tony Bowden <tony@tmtm.com>
2016-10-22 19:45:23 +0100

    remove batching
    We should be able to do all of this in one shot now

--- scraper.rb
+++ scraper.rb
@@ -5,4 +5,4 @@ require 'wikidata/fetcher'
 
 by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII legislatura della Repubblica Italiana', 'it').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat }, batch_size: 100)
+EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat })

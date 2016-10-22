ff62543af8c2db2d3931becc331dd149e80e24f6
Tony Bowden <tony@tmtm.com>
2016-10-22 19:44:57 +0100

    Update to new Category location
    Change from 'Legislatura' to 'legislatura'

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,6 @@
 
 require 'wikidata/fetcher'
 
-by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII Legislatura della Repubblica Italiana', 'it').member_titles
+by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII legislatura della Repubblica Italiana', 'it').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat }, batch_size: 100)

267f569069756a4fe9fb5a84cfb9a96899994001
Tony Bowden <tony@tmtm.com>
2018-03-23 07:04:19 +0000

    Also find via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,13 @@ require 'wikidata/fetcher'
 by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII legislatura della Repubblica Italiana', 'it').member_titles |
          WikiData::Category.new( 'Categoria:Senatori della XVIII legislatura della Repubblica Italiana', 'it').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat })
+# Senators of the 17th/18th Legislature
+sparq = <<EOQ
+  SELECT DISTINCT ?item WHERE {
+    VALUES ?term { wd:Q5487948 wd:Q48799610 }
+    ?item p:P39 [ ps:P39 wd:Q13653224 ; pq:P2937 ?term ]
+  }
+EOQ
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { it: by_cat })

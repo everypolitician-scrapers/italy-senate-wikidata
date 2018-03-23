a2bc646cacc011a19310cf31a404e5aa3d3bd371
Tony Bowden <tony@tmtm.com>
2018-03-23 07:02:26 +0000

    Also check category for 18th term

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,7 @@
 
 require 'wikidata/fetcher'
 
-by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII legislatura della Repubblica Italiana', 'it').member_titles
+by_cat = WikiData::Category.new( 'Categoria:Senatori della XVII legislatura della Repubblica Italiana', 'it').member_titles |
+         WikiData::Category.new( 'Categoria:Senatori della XVIII legislatura della Repubblica Italiana', 'it').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { it: by_cat })
